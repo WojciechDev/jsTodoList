c457ba0796496ea2b06f6a8403f6822fceb0656d
Wojciech Szmelczerczyk <szmelczerczykwojciech@gmail.com>
2021-03-12 22:26:03 +0100

    todo list with function which add tasks to list, function which search certain tasks from list not working properly yet

---
+++ script.js
@@ -0,0 +1,40 @@
+//download input for adding tasks
+const addTaskInput = document.querySelector('.input1');
+//download button which confirm adding tasks
+const addBtn = document.querySelector('.addBtn');
+//download ul to which will be added tasks
+const ul = document.querySelector('ul');
+//download input for searching tasks
+const searchTaskInput = document.querySelector('.input2');
+//li array
+let liArr = [];
+
+
+// function which add tasks to list
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = addTaskInput.value;
+    if(!(titleTask == "")){
+    const task = document.createElement('li');
+    task.innerHTML = titleTask;
+    ul.appendChild(task);
+    liArr.push(task);
+    console.log(liArr);
+    addTaskInput.value = "";
+    }else alert("Your task is empty string!");
+}
+
+
+// function which search certain tasks
+const searchTask = () => {
+    const searchText = searchTaskInput.value;
+    liArr = liArr.filter(li=>li.textContent.includes(searchText));
+    ul.textContent = "";
+    liArr.forEach(li => ul.appendChild(li));
+}
+
+//adding tasks to list
+addBtn.addEventListener('click', addTask);
+
+//searching filtered tasks
+searchTaskInput.addEventListener('input', searchTask);
