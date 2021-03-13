ea1a04a7d11fbfc7fa51495a6b709c1798e1b9a1
Matteo Crippa <matteocrippa@users.noreply.github.com>
2021-03-13 22:15:15 +0100

    introduce remove task

--- script.js
+++ script.js
@@ -10,18 +10,74 @@ const searchTaskInput = document.querySelector('.input2');
 let liArr = [];
 // list of filtered array
 let listFiltered = [];
+// shared identifier for tasks
+let taskUniqueId = 0;
+
+// function to render the list
+const renderList = (list) => {
+  console.log(`rendering: ${list.length} items`);
+  // clean the ul first
+  ul.innerHTML = '';
+  // then populate
+  list.forEach((li) => ul.appendChild(li));
+};
+
+// remove task
+const removeTask = (id, render = false) => {
+  console.log(`removing ${id}`);
+
+  // will filter the array keeping all the ids
+  // that are different by the id
+  liArr = liArr.filter((li) => {
+    console.log(li.id);
+    // need to cast both to same type (string) to
+    // have proper comparision
+    return `${li.id}` !== `${id}`;
+  });
+
+  console.log(liArr);
+
+  // do we want to render?
+  if (render) {
+    renderList(liArr);
+  }
+};
 
 // function which add tasks to list
 const addTask = (e) => {
   e.preventDefault();
   const titleTask = addTaskInput.value;
+  // do we have a task?
   if (!(titleTask == '')) {
-    const task = document.createElement('li');
+    // increase the unique id
+    taskUniqueId += 1;
+
+    // create a li
+    const liTask = document.createElement('li');
+    // ad assing the new unique id
+    liTask.setAttribute('id', taskUniqueId);
+
+    // add a now the remove button
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = 'x';
+    // assing the onclick action
+    // referring to the taskid assigned
+    // and ask to refresh
+    removeButton.setAttribute('onclick', `removeTask(${taskUniqueId}, true)`);
+
+    const task = document.createElement('strong');
     task.innerHTML = titleTask;
-    ul.appendChild(task);
-    liArr.push(task);
+
+    liTask.appendChild(task);
+    liTask.appendChild(removeButton);
+
+    liArr.push(liTask);
     console.log(liArr);
+    // reset the add
     addTaskInput.value = '';
+
+    // render again the list
+    renderList(liArr);
   } else {
     alert('Your task is empty string!');
   }
@@ -35,9 +91,9 @@ const searchTask = () => {
       li.textContent.toLowerCase().includes(searchText.toLowerCase())
     );
     ul.textContent = '';
-    listFiltered.forEach((li) => ul.appendChild(li));
+    renderList(listFiltered);
   } else {
-    liArr.forEach((li) => ul.appendChild(li));
+    renderList(liArr);
   }
 };
 
